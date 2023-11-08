c60a148a059ccf39e12f2c354dd8411db286c318
HFxLhT8JqeU5BnUG <76640703+HFxLhT8JqeU5BnUG@users.noreply.github.com>
2023-11-08 09:46:18 -0600

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -21,3 +21,19 @@ sudo docker run hello-world
 sh <(curl -sSf https://downloads.nordcdn.com/apps/linux/install.sh)
 sudo usermod -aG nordvpn $USER
 nordvpn set analytics 0 && nordvpn set technology OpenVPN && nordvpn set protocol tcp && nordvpn set autoconnect 1 && nordvpn set notify 1 && nordvpn set lan-discovery 1 && nordvpn set threatprotectionlite 1 && nordvpn set killswitch 1
+
+echo '#!/bin/bash 
+ssh -p 5163 tate@tate-server.ddns.net' > /usr/bin/tate-server
+echo '#!/bin/bash 
+ssh -p 5163 tate@our-host.ddns.net' > /usr/bin/our-host
+
+echo '#!/bin/bash
+docker stop $(docker ps -a -q)' > /usr/bin/d-stopall
+echo '#!/bin/bash
+docker rm $(docker ps -a -q)' > /usr/bin/d-stopall
+
+sudo chmod +x /usr/bin/d-stopall
+sudo chmod +x /usr/bin/d-rmall
+
+sudo chmod +x /usr/bin/tate-server
+sudo chmod +x /usr/bin/our-host
